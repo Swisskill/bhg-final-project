59d1f986bc1cc1e3ef1d87a0f4a13e7ec7b486d9
Swisskill <wbrant@uwyo.edu>
2022-04-28 15:27:07 -0600

    main contains the start of encoding a payload

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"log"
+	"os"
+)
+
+func main() {
+	pull, x := os.Open("getFiles.go")
+	if x != nil {
+		log.Fatal(x)
+	}
+	preProcess(pull)
+}
+
+func preProcess(dat *os.File) (*bytes.Reader, error) {
+	stats, err := dat.Stat()
+	if err != nil {
+		return nil, err
+	}
+	var size = stats.Size()
+	b := make([]byte, size)
+	bufR := bufio.NewReader(dat)
+	_, err = bufR.Read(b)
+	bReader := bytes.NewReader(b)
+
+	return bReader, err
+}
